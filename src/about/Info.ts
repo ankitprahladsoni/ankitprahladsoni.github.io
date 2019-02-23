535fbca2ef89c4b7e39e2570b9a64de58e54acdb
ankitprahladsoni <ankitprahladsoni@gmail.com>
2019-02-23 16:32:06 -0500

    Updates

--- src/about/Info.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  fname: 'Ankit',
-  lname: 'Soni',
-  email: 'ankitprahladsoni@gmail.com',
-  phone: '(352) 745-9208',
-  bio: `Full stack senior software developer with 6 years’ experience developing applications in Java 8 and Spring. Introduced and implemented new frontend framework to improve the team’s productivity. Lead team for better coding practices in functional programming paradigm and performance improvements`,
-};
--- src/navbar/Menu.ts
+++
@@ -1,7 +0,0 @@
-export default [
-  { name: 'About', href: '#about' },
-  { name: 'Skills', href: '#skills' },
-  { name: 'Experience', href: '#experience' },
-  { name: 'Personal Projects', href: '#projects' },
-  { name: 'Education', href: '#education' },
-];
