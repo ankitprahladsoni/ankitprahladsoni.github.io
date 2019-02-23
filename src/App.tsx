7c3c542cbc37297b9416fd5e898229148b1f8515
ankitprahladsoni <ankitprahladsoni@gmail.com>
2019-02-23 15:47:12 -0500

    extract education components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Education } from './education/Education';
 import { Projects } from './projects/Projects';
 import { Experience } from './experience/Experience';
 import { Skills } from './skills/Skills';
@@ -16,44 +17,7 @@ class App extends React.Component {
           <Skills />
           <Experience />
           <Projects />
-          <section
-            className="resume-section p-3 p-lg-5 d-flex flex-column"
-            id="education"
-          >
-            <div className="my-auto">
-              <h2 className="mb-5">Education</h2>
-
-              <div className="resume-item d-flex flex-column flex-md-row mb-5">
-                <div className="resume-content mr-auto">
-                  <h3 className="mb-0">University of Florida</h3>
-                  <div className="subheading mb-1">
-                    Masters in Science
-                    <span className="resume-date text-primary">
-                      ( Jan 2018 - Dec 2019 )
-                    </span>
-                  </div>
-                  <div>Computer and Information Science</div>
-                  <p>GPA: 3.33</p>
-                </div>
-              </div>
-
-              <div className="resume-item d-flex flex-column flex-md-row">
-                <div className="resume-content mr-auto">
-                  <h3 className="mb-0">
-                    Vellore Institute of Technology, India
-                  </h3>
-                  <div className="subheading mb-1">
-                    Bachelor of Technology
-                    <span className="resume-date text-primary">
-                      ( July 2008 - May 2012 )
-                    </span>
-                  </div>
-                  <div>Information Technology</div>
-                  <p>GPA: 3.56</p>
-                </div>
-              </div>
-            </div>
-          </section>
+          <Education />
         </div>
       </>
     );
---
+++ src/education/EducationDetails.ts
@@ -0,0 +1,16 @@
+export default [
+  {
+    uni: 'University of Florida',
+    degree: 'Masters',
+    date: 'Jan 2018 - Dec 2019',
+    branch: 'Computer and Information Science',
+    gpa: '3.4',
+  },
+  {
+    uni: 'Vellore Institute of Technology, India',
+    degree: 'Bachelor of Technology',
+    date: 'July 2008 - May 2012',
+    branch: 'Information Technology',
+    gpa: '3.56',
+  },
+];
